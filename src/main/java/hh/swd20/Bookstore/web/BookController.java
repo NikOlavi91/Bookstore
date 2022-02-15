7ea1f33654e3b3e625e29636e53251f04776a95a
Niko Haapanen <haapanenniko@gmail.com>
2022-02-15 16:33:28 +0200

    harjoitus3.5

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,9 +1,5 @@
 package hh.swd20.Bookstore.web;
 
- 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,30 +11,38 @@ import hh.swd20.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
-	
+
 	@Autowired
 	private BookRepository repository;
 
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
-	public String aloitusSivu(Model model){
+	public String aloitusSivu(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
-	
+
 	}
+
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
-		return "addbook";	
+		return "addbook";
 	}
-	
-	  @RequestMapping(value = "/save", method = RequestMethod.POST)
-	    public String save(Book book){
-	        repository.save(book);
-	        return "redirect:booklist";
-}
-	  @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 	    	repository.deleteById(bookId);
 	        return "redirect:../booklist";
-	    }     
+	}
+
+	@RequestMapping(value ="edit{id}", method = RequestMethod.GET)
+	  public String editBook(@PathVariable("id")Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "editbook";
+	}
 }
